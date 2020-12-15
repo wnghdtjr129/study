9b4d5b73e22713533a55e7adf4c90c2fdb7bbfef
wnghdtjr129 <wnghdtjr129@naver.com>
2020-12-15 19:18:20 +0900

    study5

---
+++ src/study5.java
@@ -0,0 +1,23 @@
+
+public class study5 {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,4,0,3};
+		int[] arr1 = {1,4,5,0,3,2};
+		find(arr);
+	}
+	public static void find(int[] arr) {
+		int index = 0;
+		int count=0;
+		for(int i=0; i<=arr.length; i++) {
+			index = arr[index];
+			count++;
+			if(index == 0) {
+				count++;
+				break;	
+			}
+		}
+		System.out.println((count<arr.length?"False":"True"));
+	}
+
+}
